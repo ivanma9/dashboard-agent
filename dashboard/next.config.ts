264887dfce003f03fe8a24ab9256b72803c793a1
Ivan Ma <ivanma819@gmail.com>
2025-03-18 23:13:20 -0700

    Config

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/dashboard',
+  basePath: process.env.NODE_ENV === 'production' ? '/dashboard' : '',
 };
 
 export default nextConfig;

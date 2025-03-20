232b9cdc4f9fa0099609389d881c881bde1c9460
Ivan Ma <ivanma819@gmail.com>
2025-03-20 12:53:33 -0700

    Add deploy command for prisma

--- dashboard/db.ts
+++ dashboard/db.ts
@@ -2,9 +2,7 @@ const { Pool } = require('pg');
 require ('dotenv').config();
 
 const connectionPool = new Pool({
-  connectionString: process.env.NODE_ENV === 'production' 
-    ? process.env.DATABASE_URL_PROD  // Use production URL in prod
-    : process.env.DATABASE_URL,      // Use local URL in dev
+  connectionString: process.env.DATABASE_URL,      // Use local URL in dev
 });
 
 module.exports = connectionPool;
